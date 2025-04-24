08f77635811d6c61b8443e8851ef2573f7f5c0dd
Kirill2man <kirillvershov5500@gmail.com>
2025-04-24 17:48:31 +0300

    Initialized template

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+import ReactLogo from '../logo.svg';
+
+function App() {
+    return (
+        <div className="container">
+            <div>
+                <ReactLogo width={400} height={400} />
+            </div>
+            <h1>Hello!</h1>
+            <p>Just start editing src/App/App.tsx</p>
+        </div>
+    );
+}
+
+export default App;
