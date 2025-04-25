a44a540d30fa9efdf954ae132e5b44b01d56de49
Kirill2man <kirillvershov5500@gmail.com>
2025-04-25 22:03:47 +0300

    added layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { FormEvent, useRef } from 'react';
 import './App.scss';
-import ReactLogo from '../logo.svg';
+
+function Form() {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const log = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (inputRef.current.value) {
+            console.log(inputRef.current.value);
+        }
+    };
+
+    return (
+        <form onSubmit={log}>
+            <input ref={inputRef} type="text" placeholder="Name" />
+            <button type="submit">SEARCH</button>
+        </form>
+    );
+}
+
+function ListItem() {
+    return (
+        <div className="list-item">
+            <div className="info">
+                <div>Lindblom, Miss. Augusta Charlotta</div>
+                <div>ticket: 347073</div>
+            </div>
+
+            <div>F</div>
+
+            <div>NOT SURVIVED</div>
+
+            <div className="info">
+                <div className="right-align">45y</div>
+                <div>cabin: ?</div>
+            </div>
+        </div>
+    );
+}
 
 function App() {
     return (
-        <div className="container">
-            <div>
-                <ReactLogo width={400} height={400} />
+        <div className="app">
+            <div className="container">
+                <Form />
+
+                <div className="list">
+                    <ListItem />
+                </div>
             </div>
-            <h1>Hello!</h1>
-            <p>Just start editing src/App/App.tsx</p>
         </div>
     );
 }
